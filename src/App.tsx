0a096f215237cfc72a2c111f0d57d623c638720e
Volha Patsiahevich <zaprudskaya@gmail.com>
2024-12-05 14:44:32 +0100

    feat: implement about section

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Main from './main/main';
 import License from './license/license';
+import About from './about/about';
 
 function App() {
   return (
     <>
     <Main />
     <License/>
+    <About/>
     </>
   );
 }
