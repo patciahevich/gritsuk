846217140d2211c0bace0bfddb665ab40b1ea604
Volha Patsiahevich <zaprudskaya@gmail.com>
2024-12-06 11:47:06 +0100

    fix: fix scrolling

--- src/nav/nav.tsx
+++ src/nav/nav.tsx
@@ -3,11 +3,11 @@ import './nav.scss';
 function Nav() {
   return (
     <nav>
-      <a className="nav_link" id="main" href="#main">Главная</a>
-      <a className="nav_link" id="about" href="#about">Обо мне</a>
-      <a className="nav_link" id="help" href="#help">Виды помощи</a>
-      <a className="nav_link" id="salary" href="#salary">Гонорар</a>
-      <a className="nav_link" id="blog" href="#blog">Блог</a>
+      <a className="nav_link" href="#main">Главная</a>
+      <a className="nav_link" href="#about">Обо мне</a>
+      <a className="nav_link"  href="#help">Виды помощи</a>
+      <a className="nav_link" href="#salary">Гонорар</a>
+      <a className="nav_link" href="#blog">Блог</a>
     </nav>
   )
 }
