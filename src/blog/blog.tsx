471d663c46a9185e33a26645900302925bd29939
Volha Patsiahevich <zaprudskaya@gmail.com>
2024-12-13 09:16:52 +0100

    fix: fix blog size

--- src/blog/blog.tsx
+++ src/blog/blog.tsx
@@ -4,18 +4,37 @@ import './blog.scss';
 import { useState } from 'react';
 
 function Blog() {
-  const [isMore, setIsMore] = useState(false);
+
+  const [cards, setCards] = useState(6);
+  const [isAll, setIsAll] = useState(false)
+
+  const handleClick = () => {
+    if(!isAll) {
+      let n = cards + 6;
+
+      if (n >= blogContent.length) {
+        n = blogContent.length;
+        setIsAll(true)
+      }
+
+      setCards(n);
+    } else {
+      setCards(6)
+      setIsAll(false)
+    }
+   
+  }
 
   return (
     <section id='blog'>
       <h2> Блог адвоката</h2>
-      <div className={isMore ? 'blog' : "blog active"}>
+      <div className='blog'>
         {
-          blogContent.map(item => <Card post={item}/>)
+          blogContent.slice(0, cards).map(item => <Card post={item}/>)
         }
       </div>
       <div className='more'>
-        <button onClick={() => setIsMore(!isMore)}>{isMore ? 'Развернуть' : 'Свернуть'}</button>
+        <button onClick={() => handleClick()}>{isAll ? 'Свернуть' : 'Показать больше'}</button>
       </div>
       
     </section>
